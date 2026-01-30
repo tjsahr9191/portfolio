9f8ab7cfab77aa9295c6795f3f1abff6d08d1d92
sunmok <rlfhr9191@gmail.com>
2026-01-30 14:23:40 +0900

    first commit

---
+++ src/test/kotlin/sm/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package sm.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
