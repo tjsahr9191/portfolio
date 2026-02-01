9157de196b870b97784bdccb83aafbd8d9c573b6
sunmok <rlfhr9191@gmail.com>
2026-02-01 12:04:58 +0900

    Fix lint: remove extra spaces in nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -15,7 +15,8 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: ''  // In production, Ingress proxies /api/* to backend
+      // In production, Ingress proxies /api/* to backend
+      apiBase: ''
     }
   },
 
