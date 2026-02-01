9e3e3febbd144dd5b1f21f23d5c731a834749ce3
sunmok <rlfhr9191@gmail.com>
2026-02-01 19:10:19 +0900

    Fix nuxt config key order - modules before ssr

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  ssr: false,
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui'
   ],
+
+  ssr: false,
 
   colorMode: {
     preference: 'dark',
